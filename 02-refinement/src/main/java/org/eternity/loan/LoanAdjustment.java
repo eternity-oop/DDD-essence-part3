0459106cdd55f55d798b9747b7aa0a2279c8d63b
eternity <aeternum9342@gmail.com>
2025-11-07 18:13:24 +0900

    LoanAdjustment에 원래의 대출 정보 추가

--- 02-refinement/src/main/java/org/eternity/loan/LoanAdjustment.java
+++ 02-refinement/src/main/java/org/eternity/loan/LoanAdjustment.java
@@ -3,7 +3,10 @@ package org.eternity.loan;
 import org.eternity.shared.monetary.Money;
 
 public class LoanAdjustment extends LoanInvestment {
+    private LoanInvestment original;
+
     public LoanAdjustment(LoanInvestment loanInvestment, Money amount) {
         super(loanInvestment.investment(), loanInvestment.amount().plus(amount));
+        this.original = loanInvestment;
     }
 }
